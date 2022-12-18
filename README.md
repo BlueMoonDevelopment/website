63f315b8245a456a568ecdf108eee4e1fc75bfb7
TheJeterLP <joey.peter1998@gmail.com>
2022-12-18 22:42:58 +0100

    update readme and start server through express

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # NodeJS-Website
 
+
 NPM, NodeJS and PM2 need to be installed.
 On First startup, run "npm install" to install all the neccessary dependencies.
 
+# Development
+
+simply run "npm run dev" to start server and let it restart once changes are made.
+
+# Production
 Then to start the webserver just run the start.sh, to stop just run the stop.sh
 
 Pug documentation: https://pugjs.org/api/getting-started.html
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 //server.js
 
-const http = require('http');
+/**
+ * Required external modules
+ */
 const express = require('express');
 const path = require('path');
 
+/**
+ * App Variables
+ */
 const app = express();
+const port = 8080;
 app.use(express.json());
 
+/**
+ * App Configuration
+ */
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+/**
+ * Routes Definitions
+ */
 app.get('/', (req, res) => {
     res.render('index', {title: 'Hey', message: 'Hello there!'})
 });
 
-const server = http.createServer(app);
-const port = 8080;
-server.listen(port);
-
-console.log('Server running at http://127.0.0.1:8080');
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests at 127.0.0.1:${port}`);
+});
